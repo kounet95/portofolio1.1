053c59dd1f3521e232f28f10055da8c8769908fc
kounet95 <85202452+kounet95@users.noreply.github.com>
2024-10-27 14:36:08 -0400

    v2

--- front/potofolio-react-app/src/component/Competence/outilles/ouitilles.js
+++ front/potofolio-react-app/src/component/Competence/outilles/ouitilles.js
@@ -1,10 +1,31 @@
 import React from 'react';
-
+import './styles.css'; 
 const Outils = () => {
   return (
-    <div>
-      <h1>Mes Outils</h1>
-      {/* Ajoute ton contenu ici */}
+    <div className="circle-container">
+      <div className="card-body">
+        {/* Image circulaire normale */}
+        <img
+          src="https://raw.githubusercontent.com/kounet95/portofolio-images/main/symfony.png"
+          alt="Circle"
+        />
+      </div>
+
+      <div className="card-body">
+        {/* Image circulaire avec ombre */}
+        <img
+          src="https://raw.githubusercontent.com/kounet95/portofolio-images/main/javascript.png"
+          alt="Circle with shadow"
+        />
+      </div>
+
+      <div className="card-body">
+        {/* Image circulaire avec arrière-plan coloré */}
+        <img
+          src="https://raw.githubusercontent.com/kounet95/portofolio-images/main/Eco%20Systeme%20de%20Spring.png"
+          alt="Circle with background"
+        />
+      </div>
     </div>
   );
 };
